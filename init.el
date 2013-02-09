2a31655f8c58ba1a62d4e0f8394655df88b308df
Rémi Vanicat <vanicat@debian.org>
2013-02-09 18:24:03 +0100

    Use local org-mode

--- init.el
+++ init.el
@@ -5,6 +5,10 @@
 ;; This is the first thing to get loaded.
 ;;
 
+;; Use local org-mode
+
+(setq load-path (cons "/home/moi/lang/org-mode/lisp/" load-path))
+
 ;; remember this directory
 (setq starter-kit-dir
       (file-name-directory (or load-file-name (buffer-file-name))))
